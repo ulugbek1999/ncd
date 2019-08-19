aaf4fb12f61e21d358a28cd4ab9b131bb7d20951
Ulugbek <kayrat.nazov@gmail.com>
2019-08-19 17:34:47 +0500

    Issues with history templates is fixed

--- api/admin/message_template/views.py
+++ api/admin/message_template/views.py
@@ -6,6 +6,7 @@ from api.admin.message_template.serializers import MessageTemplateSerializer
 from employee.model.employee import Employee
 from message_templates.models import Template, TemplateHistory
 from partner.models import Partner
+from bs4 import BeautifulSoup
 
 
 class MessageTemplateCreateAPIView(CreateAPIView):
@@ -25,20 +26,21 @@ class EmployeeSendMessage(APIView):
         ids = [int(i) for i in request.POST.getlist('ids')[0].split(',')]
         title = request.POST.get('title')
         text = request.POST.get('text')
+        soup = BeautifulSoup(text, 'lxml')
         if action == 'sms':
             Employee.send_sms_message(ids, title, text)
             for i in ids:
-                TemplateHistory.objects.create(title=title, text=text, employee=Employee.objects.get(id=i), message_type="SMS")
+                TemplateHistory.objects.create(title=title, text=soup.text, employee=Employee.objects.get(id=i), message_type="SMS")
         elif action == 'email':
             Employee.send_email_message(ids, title, text)
             for i in ids:
-                TemplateHistory.objects.create(title=title, text=text, employee=Employee.objects.get(id=i), message_type="Email")
+                TemplateHistory.objects.create(title=title, text=soup.text, employee=Employee.objects.get(id=i), message_type="Email")
         elif action == 'sms&email':
             Employee.send_sms_message(ids, title, text)
             Employee.send_email_message(ids, title, text)
             for i in ids:
-                TemplateHistory.objects.create(title=title, text=text, employee=Employee.objects.get(id=i), message_type="SMS")
-                TemplateHistory.objects.create(title=title, text=text, employee=Employee.objects.get(id=i), message_type="Email")
+                TemplateHistory.objects.create(title=title, text=soup.text, employee=Employee.objects.get(id=i), message_type="SMS")
+                TemplateHistory.objects.create(title=title, text=soup.text, employee=Employee.objects.get(id=i), message_type="Email")
         return Response()
 
 
