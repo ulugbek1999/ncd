5240ade47cd723685a759594bc57f410edcdb26b
Ulugbek <kayrat.nazov@gmail.com>
2019-12-05 10:46:24 +0500

    Some changes

--- api/employees/views.py
+++ api/employees/views.py
@@ -1,10 +1,11 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from utils.permissions import IsEmployer
 from employee.model.employee import Employee
 from .serializers import EmployeeSerializer
 
 class EmployeesList(ListAPIView):
-    permission_classes = (IsAuthenticated, IsEmployer, )
+    # permission_classes = (IsAuthenticated, )
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
